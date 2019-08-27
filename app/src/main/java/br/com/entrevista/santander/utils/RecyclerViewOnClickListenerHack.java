dd7df50bafcee5f2dc1dc1a4b0eac0cc91eb87be
Eduardo Costa <eduardoengcosta@gmail.com>
2019-08-27 16:51:44 -0300

    Criação das validações para a tela de Login

---
+++ app/src/main/java/br/com/entrevista/santander/utils/RecyclerViewOnClickListenerHack.java
@@ -0,0 +1,7 @@
+package br.com.entrevista.santander.utils;
+
+import android.view.View;
+
+public interface  RecyclerViewOnClickListenerHack {
+    public void onClickListener(View view, int position);
+}
---
+++ app/src/main/java/br/com/entrevista/santander/utils/Staticos.java
@@ -0,0 +1,15 @@
+package br.com.entrevista.santander.utils;
+
+public class Staticos {
+    static final String API_URL = "https://bank-app-test.herokuapp.com/api/";
+
+    static final String PREF_LOGGED_IN = "preference-logged-in";
+    static final String PREF_USER_ID = "preferences-user-id";
+    static final String PREF_USER_NAME = "preference-user-name";
+    static final String PREF_USER_AGENCY = "preferences-user-agency";
+    static final String PREF_USER_ACCOUNT = "preferences-user-account";
+    static final String PREF_USER_BALANCE = "preferences-user-balance";
+
+    public static final String BUNDLE_USER_MODEL = "user-model";
+
+}
